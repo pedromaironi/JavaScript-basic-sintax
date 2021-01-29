0b1054c55ebfbe9d7cc3fe1366aab04a9e808cb1
pedroveloper <pedrom_t@hotmail.com>
2021-01-29 18:44:42 +0100

    #05 Operadores

---
+++ operadores.js
@@ -0,0 +1,24 @@
+3 + 2
+5
+50 - 10
+40
+515151515151 - 5
+515151515146
+10 * 2000000000
+20000000000
+2 - 8 / 8
+1
+2.1 - 3 -
+    0.8999999999999999 "Pedro" + "asd"
+"Pedroasd"
+var num = 5;
+undefined
+    "Pedro tiene ${num} años"
+"Pedro tiene ${num} años"
+"Pedro tiene " + $ { num } + " años"
+3 == "3"
+true
+3 === "3"
+false
+alert("hola")
+undefined
