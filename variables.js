497a81e6652a79cee6bd217d5f808ad934141066
pedroveloper <pedrom_t@hotmail.com>
2021-01-29 17:13:39 +0100

    #01Variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+//¿Qué es una variable? Es la representacion de un lugar en memoria para luego su contenido ser consultado.
+
+//Declarar
+var name;
+
+//Inicializacion
+name = "Peter"
+
+var persona = {
+    nombre = "Pedro",
+    apellido = "Toribio"
+};
+
+persona p = new persona();
+
+console.log(s.name);
